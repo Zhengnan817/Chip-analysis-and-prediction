71ae5584ae9be5267cc4367e5285b7aecc158db1
Zhengnan817 <144702539+Zhengnan817@users.noreply.github.com>
2023-12-06 13:36:29 -0500

    update file

--- src/chip_analysis_and_prediction/__main__.py
+++ src/chip_analysis_and_prediction/__main__.py
@@ -65,26 +65,26 @@ def inferences():
     cpu_table = DataProcess(my_file_path)
     df = cpu_table.view_data()
     inferences_analysis = Inferences(df)
-    while True:
-        inferences_analysis.vendor_type_plt()
 
-        inferences_analysis.vendor_type_sns()
+    inferences_analysis.vendor_type_plt()
 
-        inferences_analysis.chip_attribute()
+    inferences_analysis.vendor_type_sns()
 
-        inferences_analysis.ave_freq_type_ven()
+    inferences_analysis.chip_attribute()
 
-        inferences_analysis.freq_and_TDP()
+    inferences_analysis.ave_freq_type_ven()
 
-        inferences_analysis.process_size_date_analysis()
+    inferences_analysis.freq_and_TDP()
 
-        inferences_analysis.chip_attribute_overview()
+    inferences_analysis.process_size_date_analysis()
 
-        inferences_analysis.freq_trend()
+    inferences_analysis.chip_attribute_overview()
 
-        inferences_analysis.cpu_freq_and_attri()
+    inferences_analysis.freq_trend()
 
-        inferences_analysis.gpu_freq_and_attri()
+    inferences_analysis.cpu_freq_and_attri()
+
+    inferences_analysis.gpu_freq_and_attri()
 
 
 main()
